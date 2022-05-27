f81d2ca97307c400bb7a1e2acc4946b0c5baefc8
jongho <breakprejudice@naver.com>
2022-05-27 09:41:23 +0900

    1_commit

---
+++ lv2/maximize_modification/solution.py
@@ -0,0 +1,21 @@
+def solution(expression):
+    answer = 0
+    return answer
+
+from itertools import permutations
+
+modi = ["-","*","+"]
+
+modif = list(permutations(modi, 3))
+
+print(modif)
+
+exp = "100-200*300-500+20"
+
+# if exp in modi:
+    
+    
+ho = exp.split("*")
+
+
+
